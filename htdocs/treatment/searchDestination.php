35c3b4fa6938a69123846c73289d885c9c648f30
msusana <susymariarodri@gmail.com>
2021-05-09 23:12:46 +0200

    jour 7

--- htdocs/treatment/searchDestination.php
+++ htdocs/treatment/searchDestination.php
@@ -11,50 +11,88 @@ $ImageManager = new ImageManager($pdo);
 $allDestinationsSearch = $DestinationManager-> getListDestinationSearch($_POST['search']);
 //Destination
 foreach ($allDestinationsSearch as $destinationSearch) { ?>
-    <div class="search">
-        <div class="row">
-        <div class='col-4'>
-        <?php
-        // image
-        $imageDestinationSearch = $ImageManager->getImageByDestination($destinationSearch->getId()); 
-                foreach($imageDestinationSearch as $imageSearch){?>
-                <img src="<?=$imageSearch->getPhoto_link()?>" alt="" srcset="">
-        </div><?php } ;?>
-        <div class='col-1'></div>
-        <div class='col-7'>
-        <div class="row">
-            <div class="col-4">
+    <div class='border border-secondary m-xs-1 m-sm-3 m-md-5 p-1 rounded text-center'>
+    
+                <div class="row mt-xs-0 mt-sm-0 mt-lg-5">
+                        <div class="col-xs-12 col-lg-6">
                 <?= $destinationSearch->getLocation(); ?>
             </div>
-            <div class="col-6">
+            <div class="col-xs-12 col-lg-6">
                 <?= $destinationSearch->getPrice(); ?>
             </div>
-        </div>
-        </div>       
+                </div>
+               
          <?php 
                 //operator 
                 $destinationOperatorSearch = $OperatorManager->getOperatorByDestination($destinationSearch->getLocation()); 
-                foreach($destinationOperatorSearch as $operatorDestinationSearch){?>
-                <div class="col-4">
-                <?= $operatorDestinationSearch->getName(); ?>
-                 </div>
+                ?>
+ <div class="row mt-xs-0 mt-sm-0 mt-lg-5">
+
+                <div class="col-xs-12 col-lg-4">
+                <?= $destinationOperatorSearch[0]->getname(); ?>
+                </div>
+
+                <div class="col-xs-12 col-lg-4">
                 <?php 
-                if ($operatorDestinationSearch->getIs_premium() === false) {
-                    echo '</br>This Operator is not Premium ';
+                if ($destinationOperatorSearch[0]->getIs_Premium() === false) {
+                    echo '<img src="https://img.icons8.com/ios/25/000000/fairytale.png" alt="" srcset="">';
                 } else {
-                    echo '</br>This Operator is Premium ';
+                    echo '<img src="https://img.icons8.com/fluent/25/000000/fairytale.png" alt="" srcset=""> ';
                 } ?>
-                <?= $operatorDestinationSearch->getGrade(); ?>
-               <?php if ($operatorDestinationSearch->getIs_premium() === true) {
-                   echo $operatorDestinationSearch->getLink(); }?>
-            </div>
+               <?php if ($destinationOperatorSearch[0]->getIs_Premium() === true) {
+                   echo $destinationOperatorSearch[0]->getLink(); }?>
+            
         </div>
-    </div>
+
+        <div class="col-xs-12 col-lg-4">
+          <?php if ($destinationOperatorSearch[0]->getGrade()< 1) {
+                                echo "<i class='far fa-star'></i>
+                                    <i class='far fa-star'></i>
+                                    <i class='far fa-star'></i>
+                                    <i class='far fa-star'></i>
+                                    <i class='far fa-star'></i>";
+                            }elseif($destinationOperatorSearch[0]->getGrade() < 2){ 
+                                echo "  <i class='fa fa-star text-warning' ></i>
+                                        <i class='far fa-star'></i>
+                                        <i class='far fa-star'></i>
+                                        <i class='far fa-star'></i>
+                                        <i class='far fa-star'></i>";
+                            }elseif($destinationOperatorSearch[0]->getGrade() < 3){ 
+                                echo "  <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='far fa-star'></i>
+                                        <i class='far fa-star'></i>
+                                        <i class='far fa-star'></i>";
+                            }elseif($destinationOperatorSearch[0]->getGrade() < 4){ 
+                                echo "  <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='far fa-star'></i>
+                                        <i class='far fa-star'></i>";
+
+                            }elseif($destinationOperatorSearch[0]->getGrade() < 5){ 
+                                echo "  <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='far fa-star'></i>";
+                            }elseif($destinationOperatorSearch[0]->getGrade() < 6){ 
+                                echo "  <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>";
+                            } echo $destinationOperatorSearch[0]->getGrade();
+                        ?>
+</div>
+                        </div>
         <form action='../destination.php' method="post">
         <input type='hidden' name='id' value='<?= $destinationSearch->getId();?>'>
         <button type="submit" class="btn  cardbtn">more information</button>
     </form>
-
-
+</div>
     <?php 
-};};  ?>
+};?>
+
+
+<?php  ?>
--- htdocs/treatment/searchTourOperator.php
+++ htdocs/treatment/searchTourOperator.php
@@ -10,30 +10,74 @@ $ImageManager = new ImageManager($pdo);
 
 $operatorData = $OperatorManager->getListOperatorSearch($_POST['search']);
 foreach ($operatorData as $operatorSearch) { ?>
-    <div class="search">
+    <div class="search border border-secondary m-xs-1 m-sm-3 m-md-5 p-1 rounded text-center">
         <div class="row">
-            <div class="col-4">
+            
+        <div class="col-xs-12 col-lg-4">
                 <?= $operatorSearch->getName(); ?>
             </div>
-            <div class="col-6">
-                <?= $operatorSearch->getLink(); ?>
-                <?php if ($operatorSearch->getIs_premium() === false) {
-                    echo '</br>This Operator is not Premium ';
+
+            <div class="col-xs-12 col-lg-4">
+          <?php if ($operatorSearch->getGrade()< 1) {
+                                echo "<i class='far fa-star'></i>
+                                    <i class='far fa-star'></i>
+                                    <i class='far fa-star'></i>
+                                    <i class='far fa-star'></i>
+                                    <i class='far fa-star'></i>";
+                            }elseif($operatorSearch->getGrade() < 2){ 
+                                echo "  <i class='fa fa-star text-warning' ></i>
+                                        <i class='far fa-star'></i>
+                                        <i class='far fa-star'></i>
+                                        <i class='far fa-star'></i>
+                                        <i class='far fa-star'></i>";
+                            }elseif($operatorSearch->getGrade() < 3){ 
+                                echo "  <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='far fa-star'></i>
+                                        <i class='far fa-star'></i>
+                                        <i class='far fa-star'></i>";
+                            }elseif($operatorSearch->getGrade() < 4){ 
+                                echo "  <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='far fa-star'></i>
+                                        <i class='far fa-star'></i>";
+
+                            }elseif($operatorSearch->getGrade() < 5){ 
+                                echo "  <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='far fa-star'></i>";
+                            }elseif($operatorSearch->getGrade() < 6){ 
+                                echo "  <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>
+                                        <i class='fa fa-star text-warning' ></i>";
+                            } echo $operatorSearch->getGrade();
+                        ?>
+</div>
+
+
+ <div class="col-xs-12 col-lg-4">
+                <?php 
+                if ($operatorSearch->getIs_Premium() === false ) {
+                    echo '<img src="https://img.icons8.com/ios/25/000000/fairytale.png" alt="" srcset="">';
                 } else {
-                    echo '</br>This Operator is Premium ';
+                    echo '<img src="https://img.icons8.com/fluent/25/000000/fairytale.png" alt="" srcset=""> ';
                 } ?>
-            </div>
+               <?php if ($operatorSearch->getIs_Premium() === true ) {
+                   echo $operatorSearch->getLink(); }?>
+            
         </div>
-    </div>
 
+      
 
-    <?php $allReviews = $ReviewManager->getReviewByOperator($operatorSearch->getId());
-    echo $operatorSearch->getId();
-    echo $operatorSearch->getGrade();
-    ?>
     <form action='../operator.php' method="post">
         <input type='hidden' name='id_tour_operator' value='<?= $operatorSearch->getId();?>'>
         <button type="submit" class="btn  cardbtn">more information</button>
     </form>
-
-<?php } ; ?>
+</div>
+    </div>
+<?php } ; ?>  
